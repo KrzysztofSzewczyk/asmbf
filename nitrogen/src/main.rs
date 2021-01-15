871603e5d97db4e562b3932389726e1fa70f2c59
Kamila Szewczyk <kspalaiologos@gmail.com>
2021-01-15 20:31:31 +0100

    some initial shitcode

--- nitrogen/src/main.rs
+++ nitrogen/src/main.rs
@@ -1,3 +1,25 @@
+
+use std::env;
+use std::process;
+use std::fs;
+use std::io::Result;
+
+fn compile(input:String) -> Result<&'static str> {
+    println!("compile stub: {}", input);
+    Ok("helo")
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args:Vec<String> = env::args().collect();
+    
+    if args.len() != 2 {
+        eprintln!("nitrogen: expected a single commandline argument.");
+        process::exit(1);
+    }
+
+    let file = &args[1];
+
+    if let Ok(contents) = fs::read_to_string(file) {
+        let status = compile(contents);
+    }
 }
